1fcac609a0e61bce831a71a0a8f8a2002a0162cb
Yaroslav Bodnar <50916274+yaroslav-bodnar@users.noreply.github.com>
2019-10-19 21:01:36 +0300

    Process work

--- src/js/script.js
+++ src/js/script.js
@@ -130,7 +130,7 @@ $(document).ready(function(){
 
 
 
-	window.addEventListener('scroll', function(){
+	window.addEventListener('mousewheel', function(){
 		$('.li_for_home a').attr('style', 'color: white');
 		$('.icon_for_active-home').attr('style', 'display: none');
 		$('.li_for_about a').attr('style', 'color: white');
@@ -147,5 +147,157 @@ $(document).ready(function(){
 		$('.icon_for_active-blog').attr('style', 'display: none')
 	});
 
+
+
+
+
+
+
+
+
+// style animation for ability blocks in competenses
+
+	var target = $('.ability_blocks');
+	var targetPos = target.offset().top;
+	var winHeight = $(window).height();
+	var scrollToElem = targetPos - winHeight;
+	var show = true;
+	var targetHtml = $('.ability_animation_element_html5');
+	var targetCss = $('.ability_animation_element_css3');
+	var targetJavascript = $('.ability_animation_element_javascript');
+	var targetJquery = $('.ability_animation_element_jquery');
+	var targetBootstrap = $('.ability_animation_element_bootstrap');
+	var targetReact = $('.ability_animation_element_react');
+	var targetWp = $('.ability_animation_element_wp');
+	var targetPhp = $('.ability_animation_element_php');
+
+
+
+	$(window).scroll(function() {
+			if(!show) return false;
+
+			var winScrollTop = $(this).scrollTop();
+			if (winScrollTop > scrollToElem) {
+				//сработает когда пользователь доскроллит к элементу с #ability_blocks
+				targetHtml.addClass('start_html');
+				targetCss.addClass('start_css');
+				targetJavascript.addClass('start_javascript');
+				targetJquery.addClass('start_jquery');
+				targetBootstrap.addClass('start_bootstrap');
+				targetReact.addClass('start_react');
+				targetWp.addClass('start_wp');
+				targetPhp.addClass('start_php');
+				$({numberValue: 0}).animate({numberValue: 90}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_html5").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 90}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_css3").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 80}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_javascript").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 80}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_jquery").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 80}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_bootstrap").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 60}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_react").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 70}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_wp").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+				$({numberValue: 0}).animate({numberValue: 50}, {
+							duration: 2000, // Продолжительность анимации, где 500 = 0,5 одной секунды, то есть 500 миллисекунд
+							easing: "linear",
+							step: function(val) {
+								$(".ability_number-procent_php").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+							}
+							
+				});
+
+				show = false;
+			}
+	});
+
+
+	// var scrollAboutTop = $('#about');
+	// var aboutPos = scrollAboutTop.offset().top;
+	// var winHeight = $(window).height();
+	// var scrollToAbout = aboutPos - winHeight;
+	// var firstHome = $(".home-background");
+	// var secondHome = $(".second_home-background");
+	// $(window).scroll(function() {
+
+	// 	var winScrollTop = $(this).scrollTop();
+	// 		if (winScrollTop > scrollToAbout) {
+	// 			secondHome.attr('style', 'display: block');
+	// 			firstHome.attr('style', 'display: none');
+	// 		}
+	// });
+
+
+
+
+	var whiteButton = $(".button_white");
+	var blackButton = $(".button_black");
+	var blockMainPortfolio = $(".main-portfolio");
+	var bootstrapPortfolioBlock = $(".bootstrap_container_for_portfolio");
+	var blockPortolioTopImageBefore = $(".portolio_top_image::before");
+	var portfolioTopImage = $(".portolio_top_image");
+
+	whiteButton.on('click', function(){
+		blockMainPortfolio.addClass('white_main-portfolio');
+		blockMainPortfolio.removeClass('main-portfolio');
+		bootstrapPortfolioBlock.addClass('white_bootstrap_container_for_portfolio');
+		bootstrapPortfolioBlock.removeClass('bootstrap_container_for_portfolio');
+
+	}); 
+
+	blackButton.on('click', function(){
+		blockMainPortfolio.addClass('main-portfolio');
+		blockMainPortfolio.removeClass('white_main-portfolio');
+		bootstrapPortfolioBlock.addClass('bootstrap_container_for_portfolio');
+		bootstrapPortfolioBlock.removeClass('white_bootstrap_container_for_portfolio');
+
+	});
+
 });
+
 
---
+++ src/js/slick.script.js
@@ -0,0 +1,10 @@
+
+
+$('.multiple-items').slick({
+	infinite: false,
+	slidesPerRow: 3,
+	rows: 2,
+	appendArrows: '.portfolio_top_menu',
+	prevArrow: '<li class="portfolio_list_arrows"><span class="icon-left-open"></span>next project</li>', 
+	nextArrow: '<li class="portfolio_list_arrows">previous projects<span class="icon-right-open"></span></li>' 
+});
